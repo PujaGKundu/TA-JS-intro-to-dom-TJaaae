b457c1c9c0ce471bdbedb4000b476e480d26f769
PujaGKundu <pujakundu100@gmail.com>
2021-11-15 18:56:16 +0530

    Assignment: Manipulating Elements

--- block-BJaaeb/code/script.js
+++ block-BJaaeb/code/script.js
@@ -6,7 +6,12 @@ default value to be "text" and return the input element inside label. (create it
 */
 
 // Your code goes here
-function createInputElm(label, type = "text") {
+function createInputElm(labelMessage, type = "text") {
+  let label = document.createElement("label");
+  let input = document.createElement("input");
+  input.type = type;
+  label.innerText = labelMessage;
+  label.append(input);
   return label;
 }
 
@@ -17,6 +22,10 @@ createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></
 // 2. Do the same thing as above using string literal like `<h1>Hello</h1>`
 
 // Your code goes here
+function createInputElm(labelMessage, type = "text") {
+  let html = `<label>${labelMessage} <input type="${type}"></label>"`;
+  return html;
+}
 
 // TEST
 createInputElm('Your name'); //<label>Your name: <input type="text"></label>
@@ -25,6 +34,12 @@ createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></
 // 3. Create a function named `createList` that accept and array of data like ['Mango', 'Apple', 'Banana'] and returns
 // the html for the link like <ul> <li>Mango</li>  <li>Apple</li>  <li>Banana</li> </ul>
 // Your code goes here
+function createList(data = []) {
+  let html = `<ul>
+  ${data.map(elm => `<li>${elm}`).join("")}
+  </ul>`;
+  return html;
+}
 
 // TEST
 createList(['ALABAMA', 'ALASKA', 'HAWAII', 'KENTUCKY']);
@@ -43,6 +58,16 @@ createList(['Afghanistan', 'Antarctica', 'Congo', 'Estonia']);
 */
 
 // Your code goes here
+function createTodoList(data = []){
+  let html = `<ul>
+  ${data.map((elm) => `<li>
+    <p>Learn DOM</p>
+    <input type="checkbox" checked name="" id="">
+    <span>X</span>
+  </li>`).join("")}
+  </ul>`;
+  return html;
+}
 
 // TEST
 createTodoList([
