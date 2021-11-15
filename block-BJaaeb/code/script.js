b560ba8152c726bab746b8468f611133ec440c0a
PujaGKundu <pujakundu100@gmail.com>
2021-11-15 18:40:00 +0530

    Manipulating Elements

--- block-BJaaeb/code/script.js
+++ block-BJaaeb/code/script.js
@@ -6,6 +6,9 @@ default value to be "text" and return the input element inside label. (create it
 */
 
 // Your code goes here
+function createInputElm(label, type = "text") {
+  return label;
+}
 
 // TEST
 createInputElm('Your name'); //<label>Your name: <input type="text"></label>
