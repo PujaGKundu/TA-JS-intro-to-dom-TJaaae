acb12b43d0359dd921d37e0279db4200de089c43
PujaGKundu <pujakundu100@gmail.com>
2021-11-16 12:16:15 +0530

    People of GOT

--- block-BJaaef/code/data.js
+++ block-BJaaef/code/data.js
@@ -326,3 +326,29 @@ let got = {
     },
   ],
 };
+
+
+
+let ul = document.querySelector('ul');
+got.houses.people.forEach(peoples => {
+    let li = document.createElement('li');
+    li.classList.add('box');
+    let div = document.createElement('div');
+    div.classList.add('flex');
+    div.classList.add('justify');
+    let img = document.createElement('img');
+    img.src = peoples.image;
+    img.alt = peoples.name;
+    let h2 = document.createElement('h2');
+    h2.innerText = peoples.name;
+    div.append(img, h2);
+    let p = document.createElement('p');
+    p.innerText = peoples.description;
+    let button = document.createElement('button');
+    let a = document.createElement('a');
+    a.innerText = "Learn More!"
+    a.href = peoples.wikiLink;
+    button.append(a);
+    li.append(div, p, button);
+    ul.append(li);
+});
